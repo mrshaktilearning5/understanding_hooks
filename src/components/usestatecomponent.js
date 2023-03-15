20ad5cb5f5a9c4d3bc3262c14c9ffdcb51b8f70f
mrshaktilearning5 <mrshaktilearning5@gmail.com>
2023-03-15 18:19:12 +0530

    kindly check first task

---
+++ src/components/usestatecomponent.js
@@ -0,0 +1,36 @@
+import React, {useState} from "react";
+
+
+
+function UnderStandingHood() {
+
+    //let val = 10;
+
+    var[val, setNewValue]=useState(15);
+
+    const handleInreaseValue = () => {
+        setNewValue(val+1);
+        console.log(" increase val" + val++);
+    }
+
+    const handleDecreaseValue = () => {
+        setNewValue(val-1);
+        console.log(" increase val" + val++);
+    }
+
+
+
+
+    return (
+        <>
+            <button onClick={handleInreaseValue}>+</button>
+            <p>{val}</p>
+            <button onClick={handleDecreaseValue}>-</button>
+        </>
+    );
+
+
+
+
+}
+export default UnderStandingHood;
